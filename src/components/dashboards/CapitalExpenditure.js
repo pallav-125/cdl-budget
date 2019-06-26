f46864912d3e2b700d456733bea2bbfe57f0e3ff
pallav <pallav_125@yahoo.com>
2019-06-26 23:31:52 +0530

    added sub expenditure pie and set default selected state

--- src/components/dashboards/CapitalExpenditure.js
+++ src/components/dashboards/CapitalExpenditure.js
@@ -6,7 +6,8 @@ import capitalData from '../../data/capitalExpenditure.json';
 class CapitalExpenditure extends React.Component {
     state = {
         expenditureFunctions: {},
-        expenditureSelectedFunction: ''
+        expenditureSelectedFunction: '',
+        subExpenditureFunctions: {}
     }
 
     getPieOptions = () => {
@@ -30,7 +31,28 @@ class CapitalExpenditure extends React.Component {
         };
     }
 
-    createPieGraph = () => {
+    createSubExpenditurePie = (mainExpenditureFunction) => {
+        let subExpenditureFunctions = this.getPieOptions(); 
+        let seriesData = [];
+
+        const data = capitalData.expenditureFunctions.filter(d => {
+            return d.name === mainExpenditureFunction;
+        });
+        console.log(data[0].subExpenditureFunctions)
+
+        data[0].subExpenditureFunctions.forEach(d => {
+            seriesData.push([d.name, d.value, false]);
+        });
+
+        subExpenditureFunctions.series[0].data = [...seriesData];
+        subExpenditureFunctions.title.text = mainExpenditureFunction;
+
+        this.setState({
+            subExpenditureFunctions: subExpenditureFunctions
+        });
+    }
+
+    createMainExpenditurePie = () => {
         let expenditureFunctions = this.getPieOptions(); 
         let seriesData = [];
 
@@ -41,9 +63,12 @@ class CapitalExpenditure extends React.Component {
         expenditureFunctions.series[0].data = [...seriesData];
         expenditureFunctions.series[0].data[0][2] = true;
         expenditureFunctions.series[0].data[0].push(true);
+        expenditureFunctions.title.text = "Main Expenditure Functions";
+        this.createSubExpenditurePie(expenditureFunctions.series[0].data[0][0]);
 
         // setting click action on pie slice
         expenditureFunctions.plotOptions.series.point.events.click = (e) => {
+            this.createSubExpenditurePie(e.point.name);
             this.setState({expenditureSelectedFunction: e.point.name});
         }
 
@@ -54,7 +79,7 @@ class CapitalExpenditure extends React.Component {
     }
 
     componentDidMount() {
-        this.createPieGraph();
+        this.createMainExpenditurePie();
     }
 
     render() {
@@ -64,7 +89,7 @@ class CapitalExpenditure extends React.Component {
                     <HighchartsReact highcharts={Highcharts} options={this.state.expenditureFunctions} />
                 </div>
                 <div className="col-md-6">
-                    <p className="text-center">{this.state.expenditureSelectedFunction}</p>
+                    <HighchartsReact highcharts={Highcharts} options={this.state.subExpenditureFunctions} />
                 </div>
             </div>
         );
