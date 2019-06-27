fbc3af276e6c3eccab4c202c246b960c64c95d48
pallav <pallav_125@yahoo.com>
2019-06-27 15:29:51 +0530

    Updates

---
+++ precache-manifest.4a15a8bf58ea1b878fa76f85c2d7bfa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979f5821aa5eb1e310efda547500e757",
+    "url": "/cdl-budget/index.html"
+  },
+  {
+    "revision": "94ab052e7d21cbf59cc9",
+    "url": "/cdl-budget/static/css/main.2cd3d7d9.chunk.css"
+  },
+  {
+    "revision": "0433b168ee40a91267b4",
+    "url": "/cdl-budget/static/js/2.c1f409f2.chunk.js"
+  },
+  {
+    "revision": "94ab052e7d21cbf59cc9",
+    "url": "/cdl-budget/static/js/main.38f7a6f2.chunk.js"
+  },
+  {
+    "revision": "74f54f1101ac30e5d3f7",
+    "url": "/cdl-budget/static/js/runtime~main.ad4235ec.js"
+  }
+]);
